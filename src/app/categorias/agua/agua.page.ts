c46d46191526d6e78ebbeb5e43986d2d72e13bc1
Vézio Cantador <vezio.cantador@etec.sp.gov.br>
2023-08-17 20:29:46 -0300

    Atualização pagina agua
    Utilização do interpolação

--- src/app/categorias/agua/agua.page.ts
+++ src/app/categorias/agua/agua.page.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AguaPage implements OnInit {
 
+  listaProdutos = [
+    {nome: "Minalba", descricao: "pet de 500 ml", valor: 2.49},
+    {nome: "Minalba", descricao: "pet de 1.5 Lts", valor: 5.50},
+    {nome: "Pureza Vital", descricao: "pet de 500 ml", valor: 2.65},
+    {nome: "Pureza Vital", descricao: "pet de 1.5 Lts", valor: 6.00},
+    {nome: "Pureza Vital", descricao: "pet de 5 Lts", valor: 9.50},
+    {nome: "Pureza Vital", descricao: "copo 180 ml", valor: 1.50}
+  ]
+
   constructor() { }
 
   ngOnInit() {
