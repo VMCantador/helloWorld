de8fb24572f76325477a84b8999123682b0efce2
Vézio Cantador <vezio.cantador@etec.sp.gov.br>
2023-08-24 21:20:28 -0300

    Finalização pagina promoção

--- src/app/promocoes/promocoes.page.ts
+++ src/app/promocoes/promocoes.page.ts
@@ -11,12 +11,11 @@ export class PromocoesPage implements OnInit {
   qtdeItensCarrinho = 0;
 
   listaProdutos = [
-    {nome: "Minalba", descricao: "pet de 500 ml", valor: 2.49, img:'https://samsclub.vtexassets.com/arquivos/ids/175670-800-auto?v=638001383415000000&width=800&height=auto&aspect=true'},
-    {nome: "Minalba", descricao: "pet de 1.5 Lts", valor: 5.50, img:'https://bretas.vtexassets.com/arquivos/ids/179178-800-auto?v=638243328258030000&width=800&height=auto&aspect=true'},
-    {nome: "Pureza Vital", descricao: "pet de 500 ml", valor: 2.65, img:'https://www.imigrantesbebidas.com.br/bebida/images/products/full/2823-agua-mineral-pureza-vital-sem-gas-510-ml.jpg'},
-    {nome: "Pureza Vital", descricao: "pet de 1.5 Lts", valor: 6.00, img:'https://www.emporionestle.com.br/media/catalog/product/a/g/agua-mineral-nestle-pureza-vital.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=265&width=265&canvas=265:265'},
-    {nome: "Pureza Vital", descricao: "pet de 5 Lts", valor: 9.50, img:'https://www.nestlepurezavital.com.br/sites/g/files/xknfdk1096/files/19_NPV_550x750.png'},
-    {nome: "Cristal", descricao: "copo 180 ml", valor: 1.50, img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlXliZx3cScuqogcRDFrLsYQcRYTSf3CVDUA&usqp=CAU'}
+    {nome: "Heineken", descricao: "600 ml", valor: 6.95, img:'https://static.cestasmichelli.com.br/images/product/22642gg.jpg?ims=750x750'},
+    {nome: "Bud", descricao: "600 ml", valor: 6.95, img:'https://img.irroba.com.br/fit-in/600x600/filters:format(webp):fill(fff):quality(80)/bebfesta/catalog/produtos/teste/beb-festa-cerveja-budweiser-600ml.jpg'},
+    {nome: "Skol", descricao: "Litrão", valor: 4.50, img:'https://d2r9epyceweg5n.cloudfront.net/stores/001/043/122/products/cerveja-skol-1000-ml1-4201d3b09d8db9916f15676911862889-480-0.jpg'},
+    {nome: "Brahma", descricao: "Lata", valor: 5.00, img:'https://choppbrahmaexpress.vtexassets.com/arquivos/ids/155702-800-800?v=637353454674430000&width=800&height=800&aspect=true'},
+    {nome: "Sub-Zero", descricao: "Lata", valor: 3.85, img:'https://cdn.shoppub.io/cdn-cgi/image/w=1000,h=1000,q=80,f=auto/cenourao/media/uploads/produtos/foto/8d435efb2cb5588201.png'}
   ]
 
   constructor(public bdtemp: BdtempService) { }
